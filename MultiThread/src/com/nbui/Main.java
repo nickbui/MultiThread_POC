0832bdce92b3e91dbb0e35d4dc23a9437edcd348
Nick <nicholas.t.bui@gmail.com>
2023-01-25 23:36:50 -0600

    Added consumer method and added code to the main class to run the methods

--- MultiThread/src/com/nbui/Main.java
+++ MultiThread/src/com/nbui/Main.java
@@ -2,7 +2,36 @@ package com.nbui;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
+        Producer_consumer producerConsumer = new Producer_consumer();
+
+        Thread t1 = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                try {
+                    producerConsumer.produce();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    producerConsumer.consume();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
     }
 }
--- MultiThread/src/com/nbui/Producer_consumer.java
+++ MultiThread/src/com/nbui/Producer_consumer.java
@@ -6,6 +6,8 @@ public class Producer_consumer {
     LinkedList<Integer> testList = new LinkedList<>();
     int capacity = 2;
 
+    public Producer_consumer(){}
+
     public void produce() throws InterruptedException {
         int value = 0;
         while(true) {
@@ -15,6 +17,23 @@ public class Producer_consumer {
                 System.out.println("Producer produced-" + value);
 
                 testList.add(value++);
+
+                notify();
+
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+    public void consume() throws InterruptedException{
+        while(true){
+            synchronized (this){
+                while(testList.size() == 0)
+                    wait();
+
+                int val = testList.removeFirst();
+
+                System.out.println("Consumer consumed-" + val);
 
                 notify();
 
